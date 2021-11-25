5d50da813cc67c481784a226fda546433a960bd2
romul1508 <77800433+romul1508@users.noreply.github.com>
2021-11-25 14:04:39 +0300

    Update choose_best_gr_moution.cpp

--- choose_best_gr_moution.cpp
+++ choose_best_gr_moution.cpp
@@ -1,14 +1,16 @@
 // This file is part of ChooseBestGrMoution.
 
+// ChooseBestGrMoution provides a selection of the best city bus schedule  
+// Copyright (C) 2021 Roman Ermakov
 // ChooseBestGrMoution is free software : you can redistribute itand /or modify
-// it under the terms of the GNU Lesser Public License as published by
+// it under the terms of the GNU Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
 // ChooseBestGrMoution is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-// GNU Lesser Public License for more details.
+// GNU Public License for more details.
 
 // You should have received a copy of the GNU Lesser Public License
 // along with ChooseBestGrMoution.If not, see < http://www.gnu.org/licenses/>.
